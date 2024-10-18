36796da4bd62d825ff7f778485614a64e8da2469
kensyone <kennynio040@gmail.com>
2024-10-18 22:09:26 +0800

    code link

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -176,7 +176,7 @@ const Divider = styled.div<{ horizontal?: boolean }>`
   height: ${props => (props.horizontal ? '100%' : 'auto')};
 `
 
-const CODE_LINK = 'https://github.com/SwaprHQ'
+const CODE_LINK = 'https://github.com/kensyone/swapr-fork-dogechain'
 
 export function Settings({ simple }: { simple?: boolean }) {
   const open = useModalOpen(simple ? ApplicationModal.SIMPLE_SETTINGS : ApplicationModal.SETTINGS)
